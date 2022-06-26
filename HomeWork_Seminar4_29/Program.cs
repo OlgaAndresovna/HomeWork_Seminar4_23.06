98915c5405a6c55fbf9635b1d4955864d30435ea
Olga <olg200919@ya.ru>
2022-06-26 10:42:08 +0300

    Задача 29  семинар от 23.06

---
+++ HomeWork_Seminar4_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Массив из 8 элементов: ");
+
+int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+PrintArray(SetArray(N));
